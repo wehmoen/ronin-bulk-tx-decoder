4bbe7749b0e26426f8ff794660acfa03e79c4dfd
Nico <mail@wehmoen.dev>
2022-07-25 19:31:06 +0200

    Add multi progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #[macro_use]
 extern crate fstrings;
 
-use std::fs;
-use futures::stream::{StreamExt};
-use indicatif::{ProgressBar, ProgressStyle};
+use std::{fs, thread};
+use std::time::Duration;
 
+use futures::stream::StreamExt;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use mongodb::{
     bson::DateTime,
     bson::doc,
-    options::FindOptions,
     Client as mongo,
     Collection,
+    options::FindOptions,
 };
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -69,7 +70,13 @@ async fn get_tx(client: &Client, hash: String) -> DecodedTransaction {
 
 #[tokio::main]
 async fn main() {
+    let multi = MultiProgress::new();
+    let style = ProgressStyle::default_spinner().template("{spinner}{bar:80.cyan/blue} {percent:>3}% | [{eta_precise}][{elapsed_precise}] ETA/Elapsed | {msg}{pos:>5}/{len:4}").unwrap();
+
     let addresses: Vec<String> = fs::read_to_string("./addresses.txt").unwrap_or("".to_string()).split(LINE_ENDING).map(str::to_string).collect();
+
+    let pb_global = multi.add(ProgressBar::new(addresses.len() as u64));
+    pb_global.set_style(style.clone());
 
     let client: mongodb::Client = mongo::with_uri_str("mongodb://127.0.0.1").await.unwrap();
     let database = client.database("ronin");
@@ -80,50 +87,47 @@ async fn main() {
 
     let http_client: Client = Client::new();
 
-    let  mut final_output: Vec<AddressDict> = vec![];
+    let mut final_output: Vec<AddressDict> = vec![];
 
     if addresses.len() > 0 {
-        println!("Found {} addresses", addresses.len());
+        println!("Found {} addresses:\n", addresses.len());
 
         for address in addresses {
+            pb_global.set_message(format!("Processing {}", address.clone()));
 
             let mut address_output: AddressDict = AddressDict {
                 address: address.clone(),
-                tx: vec![]
+                tx: vec![],
             };
 
             let txs_num = &collection.count_documents(doc! {"from": address.clone().to_lowercase().trim()}, count_options.clone()).await.unwrap();
+
+            let pb_task = multi.insert_before(&pb_global, ProgressBar::new(*txs_num));
+            pb_task.set_style(style.clone());
+
             let mut txs = collection.find(doc! {"from": address.clone().to_lowercase().trim()}, find_options.clone()).await.unwrap();
-
-            let bar = ProgressBar::new(*txs_num);
-
-            bar.set_style(
-                ProgressStyle::default_spinner().template("{spinner}{bar:80.cyan/blue} {percent:>3}% | [{eta_precise}][{elapsed_precise}] ETA/Elapsed | {msg}{pos:>5}/{len:4}").unwrap()
-            );
-
-            println!("Processing {} with {} txs", &address.to_lowercase().trim(), txs_num);
 
             while let Some(tx) = txs.next().await {
                 let tx = tx.unwrap().hash;
-                bar.set_message(f!("Decoding: {}", tx.clone()));
+                pb_task.set_message(format!("Decoding: {}", tx.clone()));
 
                 let decoded = get_tx(&http_client, tx).await;
 
                 address_output.tx.push(decoded);
-                bar.inc(1);
+                pb_task.inc(1);
             }
 
-            bar.set_position(*txs_num);
-            bar.finish();
+            pb_task.set_position(*txs_num);
+            pb_task.finish();
 
             final_output.push(address_output);
-
+            pb_global.inc(1);
         }
 
         fs::write("output.json", serde_json::to_string(&final_output).unwrap()).ok();
         print!("Saved output to output.json!");
-
     } else {
         println!("No addresses found!")
     }
+
 }
