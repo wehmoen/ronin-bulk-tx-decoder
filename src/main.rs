c5bd839a3d998972979243f8a855e5ffed43fa76
Nico <mail@wehmoen.dev>
2022-07-25 17:37:06 +0200

    Fix line ending for splitting

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ use serde_json::Value;
 const URL_BASE_DECODE_RECEIPT: &str = "http://localhost:3000/ronin/decodeTransactionReceipt/";
 const URL_BASE_DECODE_INPUT: &str = "http://localhost:3000/ronin/decodeTransaction/";
 
+#[cfg(windows)]
+const LINE_ENDING: &'static str = "\r\n";
+#[cfg(not(windows))]
+const LINE_ENDING: &'static str = "\n";
+
 #[derive(Debug, Serialize, Deserialize)]
 struct DecodedTransaction {
     hash: String,
@@ -63,7 +68,7 @@ async fn get_tx(client: &Client, hash: String) -> DecodedTransaction {
 
 #[tokio::main]
 async fn main() {
-    let addresses: Vec<String> = fs::read_to_string("./addresses.txt").unwrap_or("".to_string()).split("\r").map(str::to_string).collect();
+    let addresses: Vec<String> = fs::read_to_string("./addresses.txt").unwrap_or("".to_string()).split(LINE_ENDING).map(str::to_string).collect();
 
     let client: mongodb::Client = mongo::with_uri_str("mongodb://127.0.0.1").await.unwrap();
     let database = client.database("ronin");
